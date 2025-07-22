cc7bcb093f29b5b7b777ea8020dcd015b3aef32b
User <user@example.com>
2025-07-22 16:48:31 -0400

    add connectors and eventbridge

--- modules/transfer-connectors/outputs.tf
+++ modules/transfer-connectors/outputs.tf
@@ -1,9 +1,28 @@
-output "server_id" {
-  description = "The ID of the transfer server"
-  value       = aws_transfer_server.transfer_server.id
+#####################################################################################
+# Outputs for AWS Transfer Family SFTP Connector Module
+#####################################################################################
+
+output "connector_id" {
+  description = "The ID of the AWS Transfer Family connector"
+  value       = aws_transfer_connector.sftp_connector.id
 }
 
-output "server_endpoint" {
-  description = "The endpoint of the created Transfer Family server"
-  value       = aws_transfer_server.transfer_server.endpoint
+output "connector_arn" {
+  description = "The ARN of the AWS Transfer Family connector"
+  value       = aws_transfer_connector.sftp_connector.arn
+}
+
+output "connector_url" {
+  description = "The URL of the SFTP server the connector connects to"
+  value       = aws_transfer_connector.sftp_connector.url
+}
+
+output "connector_role_arn" {
+  description = "The ARN of the IAM role used by the connector"
+  value       = aws_iam_role.connector_role.arn
+}
+
+output "connector_logging_role_arn" {
+  description = "The ARN of the IAM role used for connector logging (if created)"
+  value       = var.logging_role != null ? var.logging_role : (length(aws_iam_role.connector_logging_role) > 0 ? aws_iam_role.connector_logging_role[0].arn : null)
 }
