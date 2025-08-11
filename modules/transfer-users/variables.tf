507b75644f330dbeb5b95e16ed43f0ad2d6141d0
iam-vignesh <vigneshraviav@gmail.com>
2025-08-11 16:49:49 -0400

    support multi keys

--- modules/transfer-users/variables.tf
+++ modules/transfer-users/variables.tf
@@ -5,21 +5,49 @@ variable "create_test_user" {
 }
 
 variable "users" {
-  description = "List of SFTP users"
+  description = "List of SFTP users. Each user can have either a single public_key or multiple public_keys, but not both."
   type = list(object({
-    username   = string
-    home_dir   = string
-    public_key = string
-    role_arn   = optional(string)
+    username    = string
+    home_dir    = string
+    public_key  = optional(string)
+    public_keys = optional(list(string))
+    role_arn    = optional(string)
   }))
   default = []
 
   validation {
     condition = alltrue([
       for user in var.users :
-      can(regex("^(ssh-rsa|ecdsa-sha2-nistp256|ecdsa-sha2-nistp384|ecdsa-sha2-nistp521|ssh-ed25519) AAAA[A-Za-z0-9+/]+[=]{0,3}( .+)?$", user.public_key))
+      !(user.public_key != null && user.public_keys != null)
     ])
-    error_message = "Public key must be in the format '<key-type> <base64-encoded-key> [comment]' where key-type is one of: ssh-rsa (including rsa-sha2-256 and rsa-sha2-512), ecdsa-sha2-nistp256, ecdsa-sha2-nistp384, ecdsa-sha2-nistp521, or ssh-ed25519. The comment is optional."
+    error_message = "Cannot specify both 'public_key' and 'public_keys' for the same user. Use either single 'public_key' or multiple 'public_keys'."
+  }
+
+  validation {
+    condition = alltrue([
+      for user in var.users :
+      try(length(user.public_keys), 0) <= 10
+    ])
+    error_message = "Maximum of 10 public keys allowed per user as per AWS Transfer Family limits."
+  }
+
+  validation {
+    condition = alltrue([
+      for user in var.users :
+      user.public_keys == null || try(length(user.public_keys) == length(distinct(user.public_keys)), true)
+    ])
+    error_message = "Duplicate public keys are not allowed for the same user."
+  }
+
+  validation {
+    condition = alltrue(flatten([
+      for user in var.users : [
+        for key in (user.public_keys != null ? user.public_keys : 
+                    user.public_key != null ? [user.public_key] : []) :
+        can(regex("^(ssh-rsa|ecdsa-sha2-nistp256|ecdsa-sha2-nistp384|ecdsa-sha2-nistp521|ssh-ed25519) AAAA[A-Za-z0-9+/]+[=]{0,3}( .+)?$", key))
+      ]
+    ]))
+    error_message = "All public keys must be in the format '<key-type> <base64-encoded-key> [comment]' where key-type is one of: ssh-rsa (including rsa-sha2-256 and rsa-sha2-512), ecdsa-sha2-nistp256, ecdsa-sha2-nistp384, ecdsa-sha2-nistp521, or ssh-ed25519. The comment is optional."
   }
 
   validation {
