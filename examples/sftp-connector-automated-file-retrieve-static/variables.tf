67efd1db466370b81e3d8ff97e69b8949132a588
prabirsekhri <prabirsekhri@gmail.com>
2025-08-26 08:13:45 -0400

    Revert "fix: dynamodb iplementaiton in static example"
    This reverts commit 1ed435ea7356b8c490178ae53b9a86ad6ad1abbc.

--- examples/sftp-connector-automated-file-retrieve-static/variables.tf
+++ examples/sftp-connector-automated-file-retrieve-static/variables.tf
@@ -1,9 +1,3 @@
-variable "enable_dynamodb_tracking" {
-  description = "Enable DynamoDB tracking for file transfers"
-  type        = bool
-  default     = true
-}
-
 variable "aws_region" {
   description = "AWS region"
   type        = string
@@ -104,6 +98,12 @@ variable "file_paths_to_retrieve" {
     ])
     error_message = "All file paths must start with a forward slash (/)."
   }
+}
+
+variable "enable_dynamodb_tracking" {
+  description = "Enable DynamoDB table to track file transfer status"
+  type        = bool
+  default     = false
 }
 
 variable "test_connector_post_deployment" {
